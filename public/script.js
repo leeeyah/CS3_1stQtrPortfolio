45e3c137793a55b7f938f4389013620538df52ee
leeeyah <b2029ecmbeltran@pshs.edu.ph>
2025-08-05 08:54:08 +0800

    Q1 1st Non-Graded

--- public/script.js
+++ public/script.js
@@ -1,1 +1,7 @@
-// no script tag needed here daw
+let age = 13
+let maxAge = 100
+let numPerDay = 1
+let totalRequired = (numPerDay * 365) * (maxAge - age)
+
+document.write("You will need " + totalRequired + " Llao Llao to last you until the ripe old age of " + maxAge)
+document.write
