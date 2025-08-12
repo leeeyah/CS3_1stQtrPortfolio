b86efed6b2d5f585c975ce8643df70321f8e047f
leeeyah <b2029ecmbeltran@pshs.edu.ph>
2025-08-12 08:23:36 +0800

    Speed Test 1

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
-let age = window.prompt("How old are you?", "13");
+let age = window.prompt("How old are you?", "14");
 let maxAge = window.prompt("What is the maximum age you expect to live to?", "100");
 let numPerDay = window.prompt("How many of your favorite snack do you eat per day?", "2");
 let totalRequired = (numPerDay * 365) * (maxAge - age)
 
 if(confirm(`Is ${age}, ${maxAge}, and ${numPerDay} correct?`)) {
-    alert("You will need " + totalRequired + " Llao Llao to last you until the ripe old age of " + maxAge);
+    document.write("You will need " + totalRequired + " Llao Llao to last you until the ripe old age of " + maxAge);
     }
 else {
-    alert("Run the code again");
+    document.write("Run the code again");
 }
